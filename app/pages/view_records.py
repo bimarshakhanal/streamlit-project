0a1423b654b372dfd7780a432dd20524cdc757ea
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-04-19 12:42:07 +0545

    feat: Add feature to view employee records

--- app/pages/view_records.py
+++ app/pages/view_records.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.header('Employee data management.')
 st.info('View employee records')
+
+dept_df = pd.read_csv('dept.csv')
+employee_df = pd.read_csv('employee.csv')
+print(dept_df.columns)
+combined_df = employee_df.join(
+    dept_df.set_index('Deptno'), on='Deptno', how='left')
+
+st.dataframe(combined_df[['Empno', 'Employee Name',
+             'Job', 'Deptno', 'Department Name']])
