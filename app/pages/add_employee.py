7fddae63e69f7e1519ed758e41f4eabe082c9c43
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-04-19 13:03:07 +0545

    feat: Select box to choose department while add employee record

--- app/pages/add_employee.py
+++ app/pages/add_employee.py
@@ -1,30 +1,25 @@
 import streamlit as st
+import pandas as pd
 
 from utils import add_employee
 
 st.header('Employee data management.')
 st.info('Add new employee.')
 
+dept_df = pd.read_csv('dept.csv')
+dept_to_id = dict(zip(dept_df['Department Name'], dept_df['Deptno']))
+
 employee_number = st.number_input('Employee number', format='%d', step=1)
 employee_name = st.text_input("Employee name")
 employee_job = st.text_input("Employee job")
-employee_department = st.number_input(
-    "Employee job", format='%d', step=1)
-
-# st.button(label='Add record',
-#           on_click=add_employee(employee_name,
-#                                 employee_name, employee_job,
-#                                 employee_department))
+employee_department = st.selectbox(
+    label='Department', options=dept_to_id.keys())
 
 submit = st.button(label='Submit')
 if submit:
     if all([employee_number, employee_name, employee_job, employee_department]):
-        print(employee_number, employee_name,
-              employee_department, employee_job)
         add_employee(employee_number, employee_name,
-                     employee_job, employee_department)
+                     employee_job, dept_to_id[employee_department])
         st.success("New record added.")
     else:
         st.error("Please fill in all fields.")
-
-# print(employee_number, employee_name, employee_department, employee_job)
