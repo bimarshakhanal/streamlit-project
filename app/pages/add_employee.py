297f7689e00c86e50efe1f06f4a41f112b7adec3
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-04-19 12:15:31 +0545

    feat: Add feature to add new employee record

--- app/pages/add_employee.py
+++ app/pages/add_employee.py
@@ -1,4 +1,30 @@
 import streamlit as st
 
+from utils import add_employee
+
 st.header('Employee data management.')
-st.info('Add new department.')
+st.info('Add new employee.')
+
+employee_number = st.number_input('Employee number', format='%d', step=1)
+employee_name = st.text_input("Employee name")
+employee_job = st.text_input("Employee job")
+employee_department = st.number_input(
+    "Employee job", format='%d', step=1)
+
+# st.button(label='Add record',
+#           on_click=add_employee(employee_name,
+#                                 employee_name, employee_job,
+#                                 employee_department))
+
+submit = st.button(label='Submit')
+if submit:
+    if all([employee_number, employee_name, employee_job, employee_department]):
+        print(employee_number, employee_name,
+              employee_department, employee_job)
+        add_employee(employee_number, employee_name,
+                     employee_job, employee_department)
+        st.success("New record added.")
+    else:
+        st.error("Please fill in all fields.")
+
+# print(employee_number, employee_name, employee_department, employee_job)
