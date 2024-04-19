25ed809760f0b4c6bc18cead829a25acd406dbc6
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-04-19 14:24:17 +0545

    feat: Check department ID for uniqueness before creating new record

--- app/pages/add_department.py
+++ app/pages/add_department.py
@@ -1,9 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 from utils import add_department
 
 st.header('Employee data management.')
 st.info('Add new department.')
+
+# get list of department IDs to enforce unique ID
+dept_df = pd.read_csv('dept.csv', usecols=['Deptno'])
+dept_ids = dept_df['Deptno'].unique().tolist()
 
 department_number = st.number_input('Department number', format='%d', step=1)
 department_name = st.text_input("Department name")
@@ -11,8 +16,10 @@ department_location = st.text_input("Department Location")
 
 submit = st.button(label='Submit')
 if submit:
-    if all([department_number, department_name, department_location]):
+    if not all([department_number, department_name, department_location]):
+        st.error("Please fill in all fields.")
+    elif department_number in dept_ids:
+        st.error(f"Department with id {department_number} already exists.")
+    else:
         add_department(department_number, department_name, department_location)
         st.success("New record added.")
-    else:
-        st.error("Please fill in all fields.")
