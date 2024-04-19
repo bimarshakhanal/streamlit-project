939373497022d96332d9fb924084dfe8ab646c96
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-04-19 12:41:22 +0545

    feat: Add feature to add new department record

--- app/pages/add_department.py
+++ app/pages/add_department.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+from utils import add_department
 
 st.header('Employee data management.')
-st.info('Add new employee.')
+st.info('Add new department.')
+
+department_number = st.number_input('Department number', format='%d', step=1)
+department_name = st.text_input("Department name")
+department_location = st.text_input("Department Location")
+
+submit = st.button(label='Submit')
+if submit:
+    if all([department_number, department_name, department_location]):
+        add_department(department_number, department_name, department_location)
+        st.success("New record added.")
+    else:
+        st.error("Please fill in all fields.")
