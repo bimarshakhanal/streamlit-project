a9ae9bd72ff011c0b93a2ef3600444f3b4bb7202
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-04-19 14:15:56 +0545

    feat: Check employee ID for uniqueness

--- app/pages/add_employee.py
+++ app/pages/add_employee.py
@@ -6,6 +6,9 @@ from utils import add_employee
 st.header('Employee data management.')
 st.info('Add new employee.')
 
+emp_df = pd.read_csv('employee.csv')
+emp_ids = emp_df['Empno'].unique().tolist()
+
 dept_df = pd.read_csv('dept.csv')
 dept_to_id = dict(zip(dept_df['Department Name'], dept_df['Deptno']))
 
@@ -17,9 +20,11 @@ employee_department = st.selectbox(
 
 submit = st.button(label='Submit')
 if submit:
-    if all([employee_number, employee_name, employee_job, employee_department]):
+    if not all([employee_number, employee_name, employee_job, employee_department]):
+        st.error("Please fill in all fields.")
+    elif employee_number in emp_ids:
+        st.error(f"Employ with id {employee_number} already exists.")
+    else:
         add_employee(employee_number, employee_name,
                      employee_job, dept_to_id[employee_department])
         st.success("New record added.")
-    else:
-        st.error("Please fill in all fields.")
